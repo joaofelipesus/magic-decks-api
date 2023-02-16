89676d624e2ac39879e63fb47cc724061a80e189
Joao Felipe <joaofelipe1294@gmail.com>
2023-02-15 21:23:40 -0300

    feat: delegate create service

--- lib/magic_decks.ex
+++ lib/magic_decks.ex
@@ -1,9 +1,3 @@
 defmodule MagicDecks do
-  @moduledoc """
-  MagicDecks keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_deck(params), to: MagicDecks.Deck.Create, as: :call
 end
